284ac948f7491a49261080a2ec1d09570b0a55f9
Kiryl Shchasny <folkbread@gmail.com>
2025-11-26 17:52:38 +0100

    feat: add advanced algorithm to find any recommedantion matches, add helpers for that and prepare card html and css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 import { initRouter } from "./router.js";
+import { initSearch } from "./search.js";
+import { initHelpers } from "./helpers.js";
 
 window.addEventListener('DOMContentLoaded', () => {
+  const helpers = initHelpers();
+
   initRouter();
+  initSearch(helpers);
 });
